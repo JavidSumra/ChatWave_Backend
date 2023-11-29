0b4cbf1e524d28c8d6b9a99e773b89ab608fba6f
JavidSumra <javidsumra135@gmail.com>
2023-11-29 12:59:09 +0530

    Adding cors Header

--- src/main.js
+++ src/main.js
@@ -11,11 +11,23 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://chatwave-tulv.onrender.com"],
+    origin: ["http://localhost:5173", "https://chatwavejavid.netlify.app"],
     credentials: true, // Cookies Can Sent With Request
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, DELETE, PUT, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 connectDB();
 
 app.use(routes); //Routes Definition
