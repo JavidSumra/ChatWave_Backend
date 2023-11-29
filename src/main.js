2254dfd3a6fe6e4740d2c9b2de29920fd3810a07
JavidSumra <javidsumra135@gmail.com>
2023-11-29 12:49:51 +0530

    Updating Cors

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chatwavejavid.netlify.app"],
     credentials: true, // Cookies Can Sent With Request
   })
 );
