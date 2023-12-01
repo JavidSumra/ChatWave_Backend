c2a2673dee10b32b755d6388b2c44411a39ecabd
JavidSumra <javidsumra135@gmail.com>
2023-12-01 12:54:37 +0530

    Moving Socket Code in index.js

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
-const server = require("./src/main");
+const app = require("./src/main");
 const PORT = process.env.PORT || 3007;
 
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server Started on PORT : ${PORT}`);
+});
+
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://chatwavejavid.netlify.app"
+    : "http://localhost:5173";
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: [URL, "http://localhost:3007", "https://chatwavejavid.netlify.app"],
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connect", (socket) => {
+  socket.on("send_msg", ({ senderId, receiverId, msg }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("receive_msg", { senderId, msg });
+  });
+
+  socket.on("addUser", (data) => {
+    data && addUser(data, socket.id);
+    socket.emit("getUsers", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User Removed With ${socket.id}`);
+    removeUser(socket.id);
+  });
 });
--- src/main.js
+++ src/main.js
@@ -6,16 +6,9 @@ const express = require("express"),
   routes = require("./routes/routes.js");
 
 // Http Server for Socket
-const http = require("http");
-const { Server } = require("socket.io");
 
-const URL =
-  process.env.NODE_ENV === "production"
-    ? "https://chatwavejavid.netlify.app"
-    : "http://localhost:5173";
-
-const server = http.createServer(http);
-const SocketPort = process.env.PORT || 3001;
+// const server = http.createServer(http);
+// const SocketPort = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -33,50 +26,11 @@ app.use(
   })
 );
 
-const io = new Server(server, {
-  cors: {
-    origin: [URL, "http://localhost:5173", "https://chatwavejavid.netlify.app"],
-    methods: ["GET", "POST"],
-  },
-});
-
 connectDB();
 
-let users = [];
-
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-};
-
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
-
-io.on("connect", (socket) => {
-  socket.on("send_msg", ({ senderId, receiverId, msg }) => {
-    const user = getUser(receiverId);
-    io.to(user.socketId).emit("receive_msg", { senderId, msg });
-  });
-
-  socket.on("addUser", (data) => {
-    data && addUser(data, socket.id);
-    socket.emit("getUsers", users);
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`User Removed With ${socket.id}`);
-    removeUser(socket.id);
-  });
-});
-
-server.listen(SocketPort, () => {
-  console.log("Server Started on", SocketPort);
-});
+// server.listen(SocketPort, () => {
+//   console.log("Server Started on", SocketPort);
+// });
 
 app.use(routes); //Routes Definition
 module.exports = app;
